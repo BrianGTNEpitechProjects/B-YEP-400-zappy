18b14c2284606ab40654d4f534ffdc2c36601b10
Lilian ARAGO <lilian.arago@outlook.fr>
2019-06-03 15:37:22 +0200

    Common.h + Cmake removed werror

---
+++ zserver/include/common.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2018
+** File description:
+** common.h
+*/
+
+#ifndef PSU_ZAPPY_2018_COMMON_H
+#define PSU_ZAPPY_2018_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#define ERROR (-1)
+#define SUCCESS 0
+#define ERROR_EXIT 84
+#define SUCCESS_EXIT 0
+#define UNUSED __attribute__((unused))
+
+#endif
