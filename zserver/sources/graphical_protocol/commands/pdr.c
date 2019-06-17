0bba3ccd0b9688e8f63d80e116b5e98e81390408
Julian <julian.frabel@epitech.eu>
2019-06-17 20:16:57 +0200

    Added new commands files

---
+++ zserver/sources/graphical_protocol/commands/pdr.c
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2018
+** File description:
+** pdr.c
+*/
+
+/* Created the 17/06/2019 at 20:07 by jfrabel */
+
---
+++ zserver/sources/graphical_protocol/commands/pin.c
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2018
+** File description:
+** pin.c
+*/
+
+/* Created the 17/06/2019 at 20:06 by jfrabel */
+
