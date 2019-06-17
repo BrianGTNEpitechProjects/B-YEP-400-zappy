35697f746f95ded62f49732cea0727f47a4ba9e6
Brian gtn <zzz>
2019-06-17 17:33:43 +0200

    * Correct some problem & andding THREEjs to modules

--- graphical/src/index.ts
+++ graphical/src/index.ts
@@ -1,5 +1,8 @@
-import { Test } from "./test";
+import { Color, PerspectiveCamera, Scene, WebGLRenderer, Camera } from 'three';
 
-new Test("Etienne");
-new Test("Brian");
-new Test("Julian");
+var scene: Scene = new Scene();
+var camera: Camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 10000);
+var renderer: WebGLRenderer = new WebGLRenderer({antialias: true, canvas: document.getElementById('main-canvas') as HTMLCanvasElement});
+renderer.setClearColor(new Color('rgb(0, 0, 0)'));
+renderer.setSize(innerWidth, innerHeight);
+renderer.render(scene, camera);
