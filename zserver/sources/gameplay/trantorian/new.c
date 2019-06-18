421910b6b7447db629562a9e25b619c8b4b3a871
Lilian ARAGO <lilian.arago@outlook.fr>
2019-06-18 14:29:45 +0200

    Ids initialization

--- zserver/sources/gameplay/trantorian/new.c
+++ zserver/sources/gameplay/trantorian/new.c
@@ -8,6 +8,14 @@
 #include <stdlib.h>
 #include "zserver.h"
 
+static id_t get_new_id(void)
+{
+    static id_t id = 0;
+
+    id++;
+    return (id);
+}
+
 trantorian_t *accept_player(zappy_t *zap) {
     trantorian_t *res = calloc(sizeof(*res), 1);
 
@@ -15,6 +23,8 @@ trantorian_t *accept_player(zappy_t *zap) {
         handle_error_return("calloc: %s\n", 0);
         return (NULL);
     }
+    res->egg_owner = invalid_id;
+    res->id = get_new_id();
     res->base.on_disconnect = &on_disconnect;
     res->base.on_extracted = &on_extract_not_connected;
     res->zappy = zap;
