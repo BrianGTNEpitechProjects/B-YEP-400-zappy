f819b2e4dc064c4487e8c788ba75a19499513b7c
Julian <julian.frabel@epitech.eu>
2019-06-19 17:46:22 +0200

    Added pic

--- zserver/sources/gameplay/commands/incantation_valid.c
+++ zserver/sources/gameplay/commands/incantation_valid.c
@@ -8,12 +8,15 @@
 #include <stdbool.h>
 #include "network_client_user_map.h"
 #include "zcommands.h"
+#include "graphical_protocol.h"
 
 bool incantation_startable(client_user_pair_t *c, \
 __attribute__((unused)) char *a)
 {
+    trantorian_t *player = c->user;
+
     if (incantation_valid(c, a)) {
-        //TODO: Here Julian #IncantationStart
+        pic(player->zappy, player);
         return (true);
     } else {
         return (false);
