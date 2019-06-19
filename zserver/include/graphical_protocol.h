e61eded86932c46449d558d99030782b3c2bc399
Julian <julian.frabel@epitech.eu>
2019-06-19 02:00:27 +0200

    Added enw

--- zserver/include/graphical_protocol.h
+++ zserver/include/graphical_protocol.h
@@ -67,6 +67,7 @@ void pfk(zappy_t *world, trantorian_t *player_laying);
 void pdr(zappy_t *world, trantorian_t *player_dropping, e_item_t item);
 void pgt(zappy_t *world, trantorian_t *player_getting, e_item_t item);
 void pdi(zappy_t *world, trantorian_t *player_dying);
+void enw(zappy_t *world, trantorian_t *egg);
 void smg(zappy_t *world, const char *msg);
 void seg(zappy_t *world, const char *team);
 
