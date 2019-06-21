e30130e829f144657febc21b6292b6e5765916be
Julian <julian.frabel@epitech.eu>
2019-06-21 18:27:30 +0200

    Reajusted spawing rate

--- zserver/sources/global.c
+++ zserver/sources/global.c
@@ -30,11 +30,11 @@ const size_t INCANT_MSG_LEN = sizeof(INCANT_MSG);
 const double resources_per_tile = 1.0 / 4.0;
 
 const item_t item_map[TOT_ITEM_NB] = {
-{.id = FOOD,        .name = (const uint8_t *)"Food", .spawn_frequency = 5},
-{.id = LINEMATE,    .name = (const uint8_t *)"Linemate", .spawn_frequency = 5},
-{.id = DERAUMERE,   .name = (const uint8_t *)"Deraumere",.spawn_frequency = 10},
-{.id = SIBUR,       .name = (const uint8_t *)"Sibur", .spawn_frequency = 20},
-{.id = MENDIANE,    .name = (const uint8_t *)"Mendiane", .spawn_frequency = 30},
-{.id = PHIRAS,      .name = (const uint8_t *)"Phiras", .spawn_frequency = 40},
-{.id = THYSTAME,    .name = (const uint8_t *)"Thystame", .spawn_frequency = 50},
+{.id = FOOD,      .name = (const uint8_t *)"Food", .spawn_frequency = 50},
+{.id = LINEMATE,  .name = (const uint8_t *)"Linemate", .spawn_frequency = 50},
+{.id = DERAUMERE, .name = (const uint8_t *)"Deraumere", .spawn_frequency = 60},
+{.id = SIBUR,     .name = (const uint8_t *)"Sibur", .spawn_frequency = 70},
+{.id = MENDIANE,  .name = (const uint8_t *)"Mendiane", .spawn_frequency = 80},
+{.id = PHIRAS,    .name = (const uint8_t *)"Phiras", .spawn_frequency = 90},
+{.id = THYSTAME,  .name = (const uint8_t *)"Thystame", .spawn_frequency = 100},
 };
